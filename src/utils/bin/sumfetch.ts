5d485927917a1ae0f46e0e60cfcd74fbdb874882
iambeto <rscabrera@gmail.com>
2023-07-18 13:05:37 -0500

    small edit

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -34,7 +34,7 @@ const sumfetch = async (args: string[]): Promise<string> => {
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="https://www.threads.net/@${config.social.threads}" target="_blank">threads.net/@${config.social.threads}</a></u>
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓           - <u><a href="https://www.threads.net/@${config.social.threads}" target="_blank">threads.net/@${config.social.threads}</a></u>
       ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            ----------- 
         ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
             ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
